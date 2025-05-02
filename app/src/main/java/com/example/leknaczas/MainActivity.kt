7f3e73ed8ff7d288ccfb89c48c969d004dfd86f3
lichutko <kuba.liszewski5@outlook.com>
2025-05-02 17:34:24 +0200

    Edycja pliku MainActivity.kt

--- app/src/main/java/com/example/leknaczas/MainActivity.kt
+++ app/src/main/java/com/example/leknaczas/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
 import com.example.leknaczas.navigation.AppNavigation
-import com.example.leknaczas.ui.theme.LeknaczasTheme
+import com.example.leknaczas.ui.theme.LekNaCzasTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LeknaczasTheme {
+            LekNaCzasTheme {
                 AppNavigation()
             }
         }
