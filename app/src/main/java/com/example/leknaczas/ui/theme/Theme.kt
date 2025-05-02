623979a01ff49007166f10ce2caf7e6154239434
lichutko <kuba.liszewski5@outlook.com>
2025-05-02 17:25:26 +0200

    edycja pliku Theme.kt

--- app/src/main/java/com/example/leknaczas/ui/theme/Theme.kt
+++ app/src/main/java/com/example/leknaczas/ui/theme/Theme.kt
@@ -11,6 +11,14 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+// Poprawione importy kolorów
+import com.example.leknaczas.ui.theme.Purple80
+import com.example.leknaczas.ui.theme.PurpleGrey80
+import com.example.leknaczas.ui.theme.Pink80
+import com.example.leknaczas.ui.theme.Purple40
+import com.example.leknaczas.ui.theme.PurpleGrey40
+import com.example.leknaczas.ui.theme.Pink40
+
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
@@ -21,20 +29,10 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
-fun LeknaczasTheme(
+fun LekNaCzasTheme( // poprawiona nazwa na LekNaCzasTheme
     darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
